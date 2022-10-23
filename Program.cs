afb33fbc6240d61dab01423ad0947dba70baca7f
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-23 21:59:49 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,82 +1,29 @@
 ﻿using System;
 
-namespace Excursion_Calculator
+namespace Football_Kit
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int people = int.Parse(Console.ReadLine());
-            string season = Console.ReadLine();
-            double price = 0;
-            double totalPrice = 0;
-            switch (season)
+            double tshortPrice = double.Parse(Console.ReadLine());
+            double goalPrice = double.Parse(Console.ReadLine());
+
+            double pants = tshortPrice * 0.75;
+            double socks = pants * 0.20;
+            double shoes = (tshortPrice + pants) * 2;
+            double totalPrice = tshortPrice + pants + socks + shoes;
+            totalPrice *= 0.85;
+            if (totalPrice>=goalPrice)
             {
-                case "spring":
-                    //Пролет(spring)
-                    //50.00 лв.на човек
-                    // 48.00 лв.на човек
-                    if (people <= 5)
-                    {
-                        price = 50;
-                        totalPrice = people * price;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    else
-                    {
-                        price = 48;
-                        totalPrice = people * price;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    break;
-                case "summer":
-                    if (people <= 5)
-                    {
-                        price = 48.50;
-                        totalPrice = people * price;
-                        totalPrice *= 0.85;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    else
-                    {
-                        price = 45;
-                        totalPrice = people * price;
-                        totalPrice *= 0.85;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    break;
-                case "autumn":
-                    if (people <= 5)
-                    {
-                        price = 60;
-                        totalPrice = people * price;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    else
-                    {
-                        price = 49.50;
-                        totalPrice = people * price;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    break;
-                case "winter":
-                    if (people <= 5)
-                    {
-                        price = 86;
-                        totalPrice = people * price;
-                        totalPrice *= 1.08;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    else
-                    {
-                        price = 85;
-                        totalPrice = people * price;
-                        totalPrice *= 1.08;
-                        Console.WriteLine($"{totalPrice:f2} leva.");
-                    }
-                    break;
-                default:
-                    break;
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {totalPrice:f2} lv.");
+            }
+            else
+            {
+                double need = goalPrice - totalPrice;
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {need:f2} lv. more.");
             }
         }
     }
