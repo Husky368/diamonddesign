7a521e642861d8a6e9b77e5c524ee40b9d8c2529
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-24 23:29:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,62 +1,43 @@
 ﻿using System;
 
-namespace Hair_Salon
+namespace Substitute
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int goal = int.Parse(Console.ReadLine());
-            string action = Console.ReadLine();
-            int budget = 0;
-            int reached = 0;
-            while (action!="closed")
+            int k = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (int K = k; K <= 8; K++)
             {
-                switch (action)
+                string wordK = Convert.ToString(K);
+                for (int i = 9; i >= l; i--)
                 {
-                    case "haircut":
-                        string type = Console.ReadLine();
-                        if (type=="mens")
+                    string wordi = Convert.ToString(i);
+                    string wordFirst = wordK + wordi;
+                    int firstNum = int.Parse(wordFirst);
+                    for (int M = m; M <= 8; M++)
+                    {
+                        string wordM = Convert.ToString(M);
+                        for (int N = 9; N >= n; N--)
                         {
-                            budget += 15;
+                            string wordN = Convert.ToString(N);
+                            string wordSecond = wordM + wordN;
+                            int secondNum = int.Parse(wordSecond);
+                            if (firstNum == secondNum)
+                            {
+                                Console.WriteLine("Cannot change the same player.");
+                            }
+                            if (K % 2 == 0 && M % 2 == 0 && i%2==1&&N%2==1)
+                            {
+                                Console.WriteLine($"{firstNum} - {secondNum}");
+                            }
                         }
-                        else if (type == "ladies")
-                        {
-                            budget += 20;
-                        }
-                        else
-                        {
-                            budget += 10;
-                        }
-                        break;
-                    case "color":
-                        string typee = Console.ReadLine();
-                        if (typee == "touch up")
-                        {
-                            budget += 20;
-                        }
-                        else
-                        {
-                            budget += 30;
-                        }
-                        break;
-                    default:
-                        break;
+                    }
                 }
-                if (budget>=goal)
-                {
-                    Console.WriteLine("You have reached your target for the day!");
-                    Console.WriteLine($"Earned money: {budget}lv.");
-                    reached++;
-                    break;
-                }
-                action = Console.ReadLine();
-            }
-            if (reached==0)
-            {
-                int need = goal - budget;
-                Console.WriteLine($"Target not reached! You need {need}lv. more.");
-                Console.WriteLine($"Earned money: {budget}lv.");
             }
         }
     }
